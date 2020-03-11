03e0d60f40dd73b0ff2206a3acc359b58816ca2d
Sergii <sergiy.stotskiy@gmail.com>
2020-03-11 17:02:50 +0200

    fix(yaml): preserve newlines in generated yaml
    Otherwise code highlighting may have issues

--- src/types/MarkdownType.js
+++ src/types/MarkdownType.js
@@ -5,17 +5,13 @@ function isSingleParagraph(html) {
   return html.startsWith('<p>') && html.endsWith('</p>') && html.indexOf('<p>', 3) === -1;
 }
 
-function cleanHtml(html) {
-  return html.trim().replace(/>[\n\r]+</g, '><');
-}
-
 module.exports = function createYamlType(options) {
   const parser = createMdParser(options);
 
   return new yaml.Type('!md', {
     kind: 'scalar',
     construct(value) {
-      const parsed = value === null ? '' : cleanHtml(parser.render(value));
+      const parsed = value === null ? '' : parser.render(value).trim();
       return isSingleParagraph(parsed) ? parsed.slice(3, -4) : parsed;
     },
   });
